a48dcfb10ab091be5956b619bb25d45910c38049
root <root@test-pgsql-app.c.hackaton-666.internal>
2017-09-30 21:25:27 +0000

    added reverse proxy scheme fix

--- app.py
+++ app.py
@@ -1,7 +1,21 @@
 from flask import Flask, jsonify, request
 from models import Castle, Unit, db
 
+
+class ReverseProxied(object):
+    def __init__(self, app):
+        self.app = app
+
+    def __call__(self, environ, start_response):
+        scheme = environ.get('HTTP_X_SCHEME', '')
+        if scheme:
+            environ['wsgi.url_scheme'] = scheme
+        return self.app(environ, start_response)
+
+
 app = Flask(__name__)
+app.wsgi_app = ReverseProxied(app.wsgi_app)
+
 
 app.config['DEBUG'] = True
 POSTGRES = {
